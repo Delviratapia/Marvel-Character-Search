b91ce3793c276ead4325631806c95512c1d19830
Dominique <domi@pop-os.localdomain>
2024-07-23 18:47:03 -0500

    Added searchbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardComponent } from "./card/card.component";
+import { SearchbarComponent } from './searchbar/searchbar.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent, SearchbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/searchbar/searchbar.component.ts
@@ -0,0 +1,90 @@
+import {Component} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+@Component({
+  selector: 'app-searchbar',
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+  ],
+  template: `
+
+<div class="search-bar">
+  <div class="searchbar-items">
+    <button class="menu-button">
+      <span class="material-icons">menu</span>
+    </button>
+    <input type="text" placeholder="Search">
+    <!-- <input type="text" placeholder="Search"> -->
+    <button class="search-button">
+      <span class="material-icons">search</span>
+    </button>
+  </div>
+</div>
+  `,
+  styles: `
+  
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+
+  .searchbar-items {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #f0f0f0;
+      border-radius: 30px;
+      padding: 0 10px;
+      box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+      max-width: 600px;
+      margin: auto;
+      
+    }
+
+    .search-bar {
+      display: flex;
+      height:10vh;
+      justify-content: center;
+      align-items: center;
+    }
+
+    input[type="text"] {
+      flex: 1;
+      border: none;
+      background: none;
+      outline: none;
+      padding: 10px;
+      color: #333;
+      font-size: 16px;
+    }
+
+    .menu-button, .search-button {
+      background: none;
+      border: none;
+      cursor: pointer;
+      padding: 10px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    .material-icons {
+      color: #666;
+      font-size: 24px;
+    }
+
+
+  `
+})
+export class SearchbarComponent {
+  // myControl = new FormControl('');
+  // options: string[] = ['One', 'Two', 'Three'];
+}
+
+
